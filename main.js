c3a9596c54f75d61f9d4c1ab3785479f92c3cfad
Akasii-88 <138124866+Akasii-88@users.noreply.github.com>
2023-07-08 19:46:32 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,122 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+const scrollHeader = () =>{
+
+    const header = document.getElementById('header')
+
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+
+    this.scrollY >= 50 ? header.classList.add('scroll-header') 
+
+                       : header.classList.remove('scroll-header')
+
+}
+
+window.addEventListener('scroll', scrollHeader)
+
+
+
+/*=============== SERVICES MODAL ===============*/
+
+const modalViews = document querySelectorAll('.services__modal'),
+
+modalBtns = document querySelectorAll('.services__button'),
+
+modalClose = document querySelectorAll('.services__modal-close')
+
+
+
+let modal = function(modalClick){
+
+  modalViews[modalClick].classList.add('.active-modal')
+
+}
+
+
+
+modalBtns.forEach((mb i) =>{
+
+  mb.addEventListener('click', () =>{
+
+    modal(i)
+
+  })
+
+})
+
+
+
+modalClose.forEach((mc) =>{
+
+  mc.addEventListener("click", () =>{
+
+    modalViews.forEach((mv) =>{
+
+      mv.classList.remove("active-modal")
+
+    })
+
+  })
+
+})
+
+
+
+
+
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+
+let mixerPortfolio = mixitup('.work__container', {
+
+    selectors: {
+
+        target: '.work__card'
+
+    },
+
+    animation: {
+
+        duration: 300
+
+    }
+
+});
+
+
+
+/* Link active work */ 
+
+const linkWork = document.querySelectorAll('.work.item')
+
+
+
+function activeWork(){
+
+  linkWork.forEach(1= > )
+
+}
+
+
+
+/*=============== SWIPER TESTIMONIAL ===============*/
+
+
+
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+
+
+
+/*=============== LIGHT DARK THEME ===============*/ 
+
+
+
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+
+
+
